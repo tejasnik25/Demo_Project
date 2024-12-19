19ac052bf2cbcfbf4fb08f74cb01c0a3976d10dc
[Tejas Nikam] <[tejasnikam2505@gmail.com]>
2024-12-19 12:13:59 +0530

    DB Updated

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <?php 
     mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-    $con = mysqli_connect('localhost','root','','carproject');
+    $con = mysqli_connect('localhost','kamilmwg_logintest','Test@db24','kamilmwg_tutorial');
     if(!$con)
     {
         echo 'please check your Database connection';
